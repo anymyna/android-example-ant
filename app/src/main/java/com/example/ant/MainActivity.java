13a106b67f79b7544fdd9f912e7bb6f91348f438
anymyna <anymyna@126.com>
2020-12-26 22:05:22 +0800

    add MainActivity,提供各功能入口

---
+++ app/src/main/java/com/example/ant/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.ant;
+
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    String TAG = "MainActivity";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Log.d(TAG,"onCreate");
+
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ComponentName cn = new ComponentName(MainActivity.this,MMKVActivity.class) ;
+                Intent intent = new Intent() ;
+                intent.setComponent(cn) ;
+                startActivity(intent) ;
+            }
+        });
+
+
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ComponentName cn = new ComponentName(MainActivity.this,RoomDemoActivity.class) ;
+                Intent intent = new Intent() ;
+                intent.setComponent(cn) ;
+                startActivity(intent) ;
+            }
+        });
+
+        //
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+}
